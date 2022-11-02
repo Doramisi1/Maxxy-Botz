3477189f81b27967f4bba8ad6a93e1af95d1b716
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-11-02 21:50:05 +0700

    Update internet-cogan-rondom.js

--- plugins/internet-cogan-rondom.js
+++ plugins/internet-cogan-rondom.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, command }) => {
 	let url = 'https://api.lolhuman.xyz/api/random/cogan?apikey=64333e1c050ced435defe154'
 	conn.sendButton(m.chat, '*–––| Sukses |–––*', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
-handler.command = /^(cogan)$/i
+handler.help = ['coganrandom']
 handler.tags = ['internet']
-handler.help = ['cogan']
-handler.premium = false
-handler.limit = true
+handler.limit = false
+handler.command = /^(coganrandom)$/i
+
 
 export default handler
